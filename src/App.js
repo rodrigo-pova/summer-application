14d2d9325047b3f3a4cb84a40ecaf372008e3d27
Rodrigo Pova <rodrigopova94@gmail.com>
2021-10-14 18:15:47 -0300

    prepara exercício 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const LanguageContext = React.createContext({
+  language: 'pt-br',
+  toggleLanguage: () => {},
+});
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.toggleLanguage = this.toggleLanguage.bind(this);
+    this.state = {
+      language: 'english',
+      toggleLanguage: this.toggleLanguage,
+    };
+  }
+
+  toggleLanguage() {
+    this.setState((prevState) => ({
+      language:
+        prevState.language === 'pt-br'
+          ? 'english'
+          : 'pt-br',
+    }));
+  }
+
+  render() {
+    return (
+      <LanguageContext.Provider value={ this.state } >
+          <Parent />
+      </LanguageContext.Provider>
+    );
+  }
+}
+
+class Parent extends React.Component {
+  render() {
+    return (
+      <LanguageContext.Consumer>
+        {(context) => (
+          <div>
+            <Child language={ context.language } />
+            <button onClick={ context.toggleLanguage }>
+              🇬🇧/🇧🇷
+            </button>
+          </div>
+        )}
+      </LanguageContext.Consumer>
+    );
+  }
+}
+
+class Child extends React.Component {
+  render() {
+    const selectedLanguage = this.props.language;
+    
+    if (selectedLanguage === 'pt-br') {
+     return <p>Olá, mundo!</p>;
+    } if (selectedLanguage === 'english') {
+     return <p>Hello, world!</p>;
+    };
+  }
 }
 
 export default App;
